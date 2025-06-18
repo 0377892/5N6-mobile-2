09a90351fc0c641e558623484454c22cb586d46d
Pierre-Olivier Brillant <PierreOlivierBrillant@users.noreply.github.com>
2025-06-18 16:47:44 -0400

    Update lib_http.dart

--- code/image_cookies/lib/lib_http.dart
+++ code/image_cookies/lib/lib_http.dart
@@ -14,7 +14,7 @@ class SingletonDio {
   }
 
   static Future<void> signUpAndGetCookie() async {
-    await getDio().post('http://10.0.2.2:8080/api/id/signup',
-        data: '{username: "bob${DateTime.now()}", password: "123456"}');
+    await getDio().post('http://10.0.2.2:8080/id/inscription',
+        data: '{nom: "bob${DateTime.now()}", motDePasse: "123456"}');
   }
 }
